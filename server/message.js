6c11844426fbdd827aaf80b6ac9e890139bed473
NiclasTi <niclas.titius@nicti.de>
2021-02-21 04:49:00 +0100

    Message interface

---
+++ server/message.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const yargs = require('yargs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+
+const message = argv.message;
+if (!message) {
+    process.exit();
+}
+
+client.on("ready",async () => {
+    const channel = client.channels.cache.find(channel => channel.id == process.env.REPORT_CHANNEL);
+    await channel.send(message);
+    process.exit();
+});
+client.login(process.env.BOT_TOKEN);
